d495b61627c3603dc343c00d7e9e203b5fec766a
kamalkarolya <kamalkarolya@gmail.com>
2022-01-18 17:32:56 +0530

    update

--- src/app/workshops/workshops.component.data.ts
+++ src/app/workshops/workshops.component.data.ts
@@ -9,6 +9,16 @@ const workshopData = [
     "imagePath":"https://i.pinimg.com/originals/7b/e5/33/7be533a7d296a8c13d8b70b85cbb4fad.jpg",
     "time":"10:30 AM",
     "more":"INNOVA DTU in collaboration with S&S consultants brings to you an amazing opportunity to race ahead of your peers and start your journey in these amazing fields, gain insightful and practical knowledge and be a part of innovation ahead. \nTraining sessions by professionals from Accenture, Wipro , American express etc, to get you acquainted industry knowledge so that you can accelerate your career.\nExplore your field of interest and develop your skills in these popular and high in demand technologies! Happy Learning."
+    },
+    { "id":2,
+    "title":"Machine Learning for Beginners",
+    "description":"INNOVA DTU brings a beginner's workshop just for you.To all the young minds who are intrigued by Sci-fi fantasies, it's time to level up. ",
+    "date":{"year":"2021","month":"Oct","day":"1"},
+    "status":false,
+    "price":"500",
+    "imagePath":"https://i.pinimg.com/originals/e4/ef/0b/e4ef0bc0686d5f119bc9bf5c489f23f2.jpg",
+    "time":"06:00 PM",
+    "more":`It is rightly said, "Predicting the future isn't magic, it's Artificial Intelligence" and to give you a glimpse of this future, INNOVA DTU welcomes the person who was an engineer-in-making himself just three years ago, a machine learning enthusiast who's currently pursuing his Master's from Liverpool John Moores University, Mr. Prathamesh Dinkar`
     }
 ];
 
